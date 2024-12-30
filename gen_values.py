c0f74eac1c0fe4ff5fbfd553f8f2e723cb73614d
nalbam <me@nalbam.com>
2024-12-30 20:43:30 +0900

    Refactor gen_values.py to support multiple template file extensions and update environment configuration files with new AWS account ID. Remove deprecated values-template.yaml files for various charts.

--- gen_values.py
+++ gen_values.py
@@ -17,30 +17,29 @@ def parse_args():
     return p.parse_args()
 
 
-def gen_repos(args):
-    os.makedirs("build", exist_ok=True)
-    os.makedirs("charts", exist_ok=True)
-
-    template_path = "charts/{}/values-template.yaml".format(args.reponame)
+def gen_repos(args, ext="yaml"):
+    template_name = "values-template.{}".format(ext)
+    template_path = "charts/{}/{}".format(args.reponame, template_name)
 
     if os.path.exists(template_path):
         print("# gen_values", template_path)
 
         e = Environment(loader=FileSystemLoader("charts/{}/".format(args.reponame)))
-        t = e.get_template("values-template.yaml")
+        t = e.get_template(template_name)
 
-        gen_values(t, args.reponame)
+        for phase in ["alpha", "prod"]:
+            gen_values(t, args.reponame, phase)
 
 
-def gen_values(t, reponame):
-    for env_file in os.listdir("env"):
+def gen_values(t, reponame, phase):
+    for env_file in os.listdir("env/{}".format(phase)):
         if env_file.endswith(".yaml"):
             # print("")
 
-            env_path = "env/{}".format(env_file)
+            env_path = "env/{}/{}".format(phase, env_file)
             # print("# env ", env_path)
 
-            save_root = "charts/{}/demo".format(reponame)
+            save_root = "charts/{}/{}".format(reponame, phase)
             save_path = "{}/values-{}".format(save_root, env_file)
 
             with open(env_path, "r") as vars:
@@ -58,7 +57,11 @@ def gen_values(t, reponame):
 def main():
     args = parse_args()
 
-    gen_repos(args)
+    os.makedirs("build", exist_ok=True)
+    os.makedirs("charts", exist_ok=True)
+
+    gen_repos(args, "yaml")
+    gen_repos(args, "yaml.j2")
 
 
 if __name__ == "__main__":
