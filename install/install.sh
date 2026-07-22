284caf4fa0350945fd5edc5bebab677136db648a
nalbam <me@nalbam.com>
2026-07-22 12:37:10 +0900

    Fix comment to execute apps.yaml deployment in install.sh

--- install/install.sh
+++ install/install.sh
@@ -19,6 +19,6 @@ argocd proj create apps --allow-cluster-resource '*/*' --dest '*,*' --src '*'
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/opspresso/argocd-env-addons/main/addons.yaml
 
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/opspresso/argocd-env-demo/main/apps.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/opspresso/argocd-env-demo/main/apps.yaml
 
 echo "https://argocd.demo.opspresso.com"
